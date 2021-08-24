84adc47704858b947b27e21b9d27bfae9d83ccbe
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-24 18:56:35 +0700

    Default main and example data fetching in react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [emojisData, setEmojisData] = useState([]);
+  useEffect(() => {
+    async function fetchemojis(){
+      const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28')
+      console.log(res);
+    }
+
+    fetchemojis()
+  }, [])
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
