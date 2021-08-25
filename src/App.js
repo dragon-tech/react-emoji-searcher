8c6f61ee5d032731eafd946339cc486f6d5f68c4
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-25 15:41:45 +0700

    dynamic rendering based on fetching statusg

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   useEffect(() => {
-    async function fetchemojis(){
+    async function fetchemojis() {
       setLoading(true);
-      try{
-        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+      try {
+        const res = await axios.get(
+          "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
+        );
         setEmojisData(res.data);
         setLoading(false);
-      }catch (error) {
-        console.error(error);
+      } catch (error) {
         setError(true);
         setLoading(false);
+        console.error(error);
       }
     }
 
-    fetchemojis()
-  }, [])
+    fetchemojis();
+  }, []);
   return (
     <div>
       <h1>Hello, world!</h1>
+      {loading && <p>loading...</p>}
+      {error && <p>Ooooppss...</p>}
+      {emojisData.length > 0 && <p>Emojis data sukses!</p>}
     </div>
   );
 }
