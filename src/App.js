199a02d5c001574d03f6083a875e1b20dd66ba54
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-25 14:59:34 +0700

    store response data to state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import axios from "axios";
 
 function App() {
   const [emojisData, setEmojisData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     async function fetchemojis(){
-      const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28')
-      console.log(res);
+      setLoading(true);
+      try{
+        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+        setEmojisData(res.data);
+        setLoading(false);
+      }catch (error) {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      }
     }
 
     fetchemojis()
   }, [])
+
+  console.log('loading ? ', loading);
+  console.log('error ? ', error);
+  console.log('emojis data ? ', emojisData);
   return (
     <div>
       <h1>Hello, world!</h1>
